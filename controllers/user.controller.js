1014382b6f5b7c5dd50c1b375586f033244ecfae
“Housam <housam.kak20@gmail.com>
2022-10-16 23:09:35 +0300

    backend => added user controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,39 @@
+const UserModel = require("../models/user.model");
+
+const createUser = async (req, res) => {
+  UserModel.create(req.body)
+    .then((user) => res.send(user))
+    .catch((err) => res.status(400).send(err));
+};
+
+const updateUser = async (req, res) => {
+  const { id, ...data } = req.body;
+  await UserModel.findByIdAndUpdate(id, {
+    data,
+  })
+    .then((user) => res.send(user))
+    .catch((err) => res.status(400).send(err));
+};
+
+const deleteUser = async (req, res) => {
+  const { id } = req.body;
+  await UserModel.findByIdAndRemove(id).then((user) => res.send(user));
+};
+
+const getUser = async (req, res) => {
+  const { id } = req.params;
+  await UserModel.find({ id }).then((user) => res.send(user));
+};
+
+const getAllUsers = async (req, res) => {
+  const users = await UserModel.find();
+  res.send(users);
+};
+
+module.exports = {
+  createUser,
+  updateUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+};
