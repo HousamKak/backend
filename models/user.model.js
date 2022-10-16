c8817a18b9a8186ffc87ba37b49808e971603296
“Housam <housam.kak20@gmail.com>
2022-10-16 22:49:37 +0300

    backend => added the User model

---
+++ models/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: "name is required",
+  },
+  email: {
+    type: String,
+    required: "email is required",
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: "password is required",
+    select: false,
+  },
+  profile: {
+    type: String,
+    default: "default.jpg",
+  },
+  title: {
+    type: String,
+    default: " I am a new user",
+  },
+  about: {
+    type: String,
+    default: "Fill me with useful information",
+  },
+  cv: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
